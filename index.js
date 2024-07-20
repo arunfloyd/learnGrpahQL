271223f3737864ab979d25b75f77f2cda7834015
Arun Vinod K <arunvinod9497@gmail.com>
2024-07-20 22:59:34 +0530

    Created Schema and db which includes the data in it.Ready to start the server

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 
+//DB
+import db from "./_db.js";
+// types
+import { typeDefs } from "./schema";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 //server setup
 const server = new ApolloServer({
-    //typeDefs -- definitions of types of data 
-    //resolvers
-})
+  //typeDefs -- definitions of types of data
+  typeDefs,
+  //resolvers
+  resolvers,
+});
 
-const{url}= await startStandaloneServer(server,{
-    listen:{port:4000}
-})
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-console.log('Server ready at port',4000)
+console.log("Server ready at port", 4000);
